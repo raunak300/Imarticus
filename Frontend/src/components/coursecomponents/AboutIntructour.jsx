0271f1c89cafbbce4e21b3769dfe57016b527155
Raunak <raunaktomar30@gmail.com>
2025-10-03 10:44:00 +0530

    this is done

--- Frontend/src/components/coursecomponents/AboutIntructour.jsx
+++ Frontend/src/components/coursecomponents/AboutIntructour.jsx
@@ -19,11 +19,11 @@ const AboutInstructor = () => {
             <h3 className="instructor-name">Ritesh Singh</h3>
             
             <p className="instructor-bio">
-              Ritesh Singh is an **IIT Delhi Graduate** with **8 years of experience** in Web Development and has created more than **15 websites** both for his own entrepreneurial ventures as well as for other companies. With this course, he wants to disseminate this skill of making complex websites in just **30 days** to as many students as possible. So, without a second thought, **invest in yourself** if you want to learn web development.
+              Ritesh Singh is an IIT Delhi Graduate with 8 years of experience in Web Development and has created more than 15 websites both for his own entrepreneurial ventures as well as for other companies. With this course, he wants to disseminate this skill of making complex websites in just 30 days to as many students as possible. So, without a second thought, invest in yourself if you want to learn web development.
             </p>
 
             <p className="instructor-experience-footer">
-              With 2 Patents and 4 innovation report, instructor is having **8 Years of Industrial Experience**
+              With 2 Patents and 4 innovation report, instructor is having 8 Years of Industrial Experience
             </p>
           </div>
         </div>
--- Frontend/src/components/coursecomponents/FAQ.jsx
+++ Frontend/src/components/coursecomponents/FAQ.jsx
@@ -10,7 +10,7 @@ const FAQ = () => {
         <div className="faq-question-block">
           <h3 className="faq-question-text">How will I learn?</h3>
           <p className="faq-answer-text">
-            When you get access to the course, you will be able to see the **modules** within the course. Watch the videos whenever you get time and complete the **tests** for each module. The self-paced videos have been created by subject matter experts to ensure you learn from the best.
+            When you get access to the course, you will be able to see the modules within the course. Watch the videos whenever you get time and complete the tests for each module. The self-paced videos have been created by subject matter experts to ensure you learn from the best.
           </p>
           <hr className="faq-separator" />
         </div>
@@ -19,7 +19,7 @@ const FAQ = () => {
         <div className="faq-question-block">
           <h3 className="faq-question-text">Will I get a certificate?</h3>
           <p className="faq-answer-text">
-            Yes, when you complete all videos and tests in the course you will be **eligible for certification**. Your certificate will get unlocked. You can share the certificate with others on social media and add it to your profile.
+            Yes, when you complete all videos and tests in the course you will be eligible for certification. Your certificate will get unlocked. You can share the certificate with others on social media and add it to your profile.
           </p>
         </div>
       </div>
